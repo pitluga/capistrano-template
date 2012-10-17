ce4cbcd7799704452a901f3067dd0b49a7e414ec
Tony Pitluga <code@getbraintree.com>
2012-10-17 08:14:43 -0500

    adds a publish method to the plugin that does nothing useful yet

--- lib/capistrano/template/plugin.rb
+++ lib/capistrano/template/plugin.rb
@@ -6,7 +6,17 @@ module Capistrano
         ERB.new(content).result(chain.instance_eval { binding })
       end
 
+      def publish(template, location, options={})
+        raise ArgumentError.new("location must be an absolute path") unless location =~ %r(^/)
+        published_files.push(:template => template, :location => location, :options => options)
+      end
+
       private
+      def published_files
+        set(:__template_published_files, []) unless exists?(:__template_published_files)
+        fetch(:__template_published_files)
+      end
+
       def template_path
         fetch(:template_path, File.join('config', 'deploy', 'templates'))
       end
--- test/capistrano/template/plugin_test.rb
+++ test/capistrano/template/plugin_test.rb
@@ -16,4 +16,22 @@ class PluginTest < Test::Unit::TestCase
     result = @config.template.render("hello.erb")
     assert_equal(result, "Hello from #{TEST_TEMPLATES}\n")
   end
+
+  def test_publish_stores_where_templates_should_be_placed
+    @config.template.publish('hello.erb', '/tmp/hello')
+    assert_equal('hello.erb', @config.__template_published_files.first[:template])
+    assert_equal('/tmp/hello', @config.__template_published_files.first[:location])
+  end
+
+  def test_publish_fills_in_the_options_hash_for_put
+    @config.template.publish('hello.erb', '/tmp/hello', :mode => '0640', :roles => :app)
+    assert_equal(:app, @config.__template_published_files.first[:options][:roles])
+    assert_equal('0640', @config.__template_published_files.first[:options][:mode])
+  end
+
+  def test_publish_raises_an_argument_error_if_location_is_not_an_absolute_path
+    assert_raises(ArgumentError) do
+      @config.template.publish('hello.erb', 'hello', :mode => '0640', :roles => :app)
+    end
+  end
 end
